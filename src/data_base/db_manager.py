c7d007210e7b1c151ce05dfad31ea8e1fde73fb0
u_alex90 <u_alex90@mail.ru>
2023-09-09 00:39:35 +1000

    Mod DBManager

--- src/data_base/db_manager.py
+++ src/data_base/db_manager.py
@@ -1,2 +1,38 @@
+import psycopg2
+
+
 class DBManager:
-    pass
+
+    def get_companies_and_vacancies_count(self):
+        """
+        Получает список всех компаний и количество вакансий
+        у каждой компании
+        """
+        pass
+
+    def get_all_vacancies(self):
+        """
+        Получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию
+        """
+        pass
+
+    def get_avg_salary(self):
+        """
+        Получает среднюю зарплату по вакансиям
+        """
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        """
+        Получает список всех вакансий, у которых зарплата выше
+        средней по всем вакансиям
+        """
+        pass
+
+    def get_vacancies_with_keyword(self):
+        """
+        Получает список всех вакансий, у которых зарплата выше
+        средней по всем вакансиям
+        """
+        pass
